f726369e90598326ab27ac80f2ade933e3360371
David G. López <guitarrkurt@icloud.com>
2014-08-04 00:00:38 -0500

    Vamos en A

---
+++ vamosEnA.m
@@ -0,0 +1,18 @@
+#import  <Cocoa/Cocoa.h>
+@interface llegarC : NSObject
+
+@property (nonatomic) NSString *var;
+
+@end
+
+@implementation llegarC
+
+@end
+
+#import <Foundation/Foundation.h>
+int main(){
+	llegarC *nuevo = [[llegarC alloc] init];
+	nuevo.var = @" A =)";
+	NSLog(@"\nVamos en la version %@:", nuevo.var);
+return 0;
+}
